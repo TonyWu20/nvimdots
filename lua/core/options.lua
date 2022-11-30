9016861c7803c48ffaa5d77164fa51dbcda59f2f
TonyWu20 <tony.w21@gmail.com>
2022-11-30 11:38:09 +0800

    Revert `timeoutlen` to 500 to fix keymap problem

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -69,7 +69,7 @@ local function load_options()
 		switchbuf = "usetab,uselast",
 		termguicolors = true,
 		timeout = true,
-		timeoutlen = 300,
+		timeoutlen = 500,
 		ttimeout = true,
 		ttimeoutlen = 0,
 		undodir = global.cache_dir .. "/undo//",
