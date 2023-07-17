881eaf7c0d342bc331370a98c97729d55918d07f
Xie Zejian <xiezej@gmail.com>
2023-07-18 01:08:09 +0800

    workaroud for watchfile problem (#871)
    * workaroud for watchfile problem
    
    * fix CI

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -156,6 +156,16 @@ You're recommended to install PowerShell for better experience.]],
 	end
 end
 
+local _v0_10_workarounds = function()
+	local ok, watchfile = pcall(require, "vim.lsp._watchfiles")
+	if ok then
+		-- Disable lsp watcher
+		watchfile._watchfunc = function()
+			return function() end
+		end
+	end
+end
+
 local load_core = function()
 	createdir()
 	leader_map()
@@ -165,6 +175,7 @@ local load_core = function()
 	clipboard_config()
 	dashboard_config()
 	shell_config()
+	_v0_10_workarounds()
 
 	require("core.options")
 	require("core.event")
