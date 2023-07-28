dd63361291b04cad97841fac05e7e1985b8b5cd9
TonyWu20 <tony.w21@gmail.com>
2023-07-28 11:55:03 +0800

    Merge from ayamir-main

--- lua/modules/configs/editor/vimtex.lua
+++ lua/modules/configs/editor/vimtex.lua
@@ -1,8 +1,9 @@
 return function()
-	vim.g.vimtex_view_method = "zathura"
+	vim.g.vimtex_view_method = "skim"
 	vim.g.vimtex_compiler_latexmk_engines = {
 		_ = "-xelatex",
 	}
 	vim.g.tex_comment_nospell = 1
 	vim.g.vimtex_compiler_progname = "nvr"
+	vim.g.vimtex_view_skim_sync = 1
 end
