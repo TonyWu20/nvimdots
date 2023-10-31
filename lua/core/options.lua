873bfbb4db6a09c2420bc4b192edf1acb8c4d19c
TonyWu20 <tony.w21@gmail.com>
2023-10-31 21:20:11 +0800

    Adjust `winminwidth` from 1 to 5

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -83,7 +83,7 @@ local function load_options()
 		wildignorecase = true,
 		-- Do NOT adjust the following option (winblend) if you're using transparent background
 		winblend = 0,
-		winminwidth = 1,
+		winminwidth = 5,
 		winwidth = 30,
 		wrapscan = true,
 		writebackup = true,
