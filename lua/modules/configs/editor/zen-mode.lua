0bc09802ebcd264fb80062c5189551e30cf3b743
TonyWu20 <tony.w21@gmail.com>
2024-01-08 21:09:26 +0800

    Add zen-mode

---
+++ lua/modules/configs/editor/zen-mode.lua
@@ -0,0 +1,25 @@
+return function()
+	require("modules.utils").load_plugin("zen-mode", {
+		window = {
+			backdrop = 0.95,
+			width = 85,
+			height = 1,
+			options = {
+				list = false,
+				foldcolumn = "0",
+				signcolumn = "no",
+				cursorcolumn = false,
+				cursorline = false,
+			},
+		},
+		plugins = {
+			tmux = {
+				enabled = true,
+			},
+			wezterm = {
+				enabled = true,
+				font = "+0",
+			},
+		},
+	})
+end
