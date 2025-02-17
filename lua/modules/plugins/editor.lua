1c083d0464b18ee640d2e5b8e7b25362a8c243b9
TonyWu20 <tony.w21@gmail.com>
2025-02-17 14:28:02 +0800

    remove nvim-surround

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -155,14 +155,4 @@ editor["andrewferrier/wrapping.nvim"] = {
 editor["folke/zen-mode.nvim"] = {
 	config = require("editor.zen-mode"),
 }
-
-editor["kylechui/nvim-surround"] = {
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	event = "VeryLazy",
-	config = function()
-		require("nvim-surround").setup({
-			-- Configuration here, or leave empty to use defaults
-		})
-	end,
-}
 return editor
