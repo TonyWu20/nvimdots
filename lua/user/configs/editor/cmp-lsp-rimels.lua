cb5a9f08e44303cf91d2ed57325da64ca9486202
TonyWu20 <tony.w21@gmail.com>
2025-03-01 18:33:46 +0800

    Add `mini.surround` and chinese input related plugins

--- lua/user/configs/editor/cmp-lsp-rimels.lua
+++ lua/user/configs/editor/cmp-lsp-rimels.lua
@@ -2,5 +2,10 @@ return function()
 	vim.system({ "rime-ls", "--listen", "127.0.0.1:9257" }, { detach = true })
 	require("rimels").setup({
 		cmd = vim.lsp.rpc.connect("127.0.0.1", 9257),
+		cmp_keymaps = {
+			diable = {
+				punctuation_upload_directly = false,
+			},
+		},
 	})
 end
--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -104,4 +104,34 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 	},
 }
 
+editor["echasnovski/mini.surround"] = {
+	version = false,
+	config = require("configs.editor.surround"),
+}
+
+editor["kealsing/im-select.nvim"] = {
+	lazy = false,
+	opts = {
+		default_im_select = "keyboard-us",
+		default_command = "fcitx5-remote",
+	},
+}
+editor["rainzm/flash-zh.nvim"] = {
+	event = "VeryLazy",
+	dependencies = "folke/flash.nvim",
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash-zh").jump({
+					chinese_only = false,
+				})
+			end,
+			desc = "Flash between Chinese",
+		},
+	},
+}
+editor["noearc/jieba.nvim"] = { dependencies = { "noearc/jieba-lua" }, opts = {} }
+
 return editor
