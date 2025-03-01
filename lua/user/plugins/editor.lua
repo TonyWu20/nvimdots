0934ba05822117f90a9903e9dad5b96e99ed7c6f
TonyWu20 <tony.w21@gmail.com>
2025-03-01 19:03:36 +0800

    Adjust `cmp-lsp-rimels` installation

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -1,9 +1,5 @@
 local editor = {}
 
-editor["liubianshi/cmp-lsp-rimels"] = {
-	-- keys = { { "<localleader>f", mode = "i" } },
-	config = require("configs.editor.cmp-lsp-rimels"),
-}
 
 editor["echasnovski/mini.surround"] = {
 	version = false,
