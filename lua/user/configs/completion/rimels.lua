d57f2bca1757c294a6cb35b5d8a5763f08a53d7d
TonyWu20 <tony.w21@gmail.com>
2025-03-01 19:08:15 +0800

    Fix typo

--- lua/user/configs/completion/rimels.lua
+++ lua/user/configs/completion/rimels.lua
@@ -1,10 +1,10 @@
 return function()
-	vim.system({ "rime-ls", "--listen", "127.0.0.1:9257" }, { detach = true })
+	vim.system({ "rime_ls", "--listen", "127.0.0.1:9257" }, { detach = true })
 	require("rimels").setup({
 		cmd = vim.lsp.rpc.connect("127.0.0.1", 9257),
 		cmp_keymaps = {
 			disable = {
-				punctuation_upload_directly = false,
+				punctuation_upload_directly = true,
 			},
 		},
 		rime_user_dir = "~/.local/share/rime-ls",
