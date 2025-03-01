d1c512ad40d07f7705394f5542bae4363089762d
TonyWu20 <tony.w21@gmail.com>
2025-03-01 19:08:15 +0800

    Fix typo

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -3,6 +3,7 @@ completion["hrsh7th/nvim-cmp"] = {
 	dependencies = {
 		{
 			"liubianshi/cmp-lsp-rimels",
+			keys = { { "<localleader>f", mode = "i" } },
 			config = require("configs.completion.rimels"),
 		},
 	},
