0bd8da667dfacc3634065af7425a6fb26537f424
TonyWu20 <tony.w21@gmail.com>
2025-03-01 18:33:46 +0800

    Add `mini.surround` and chinese input related plugins

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -1,8 +1,38 @@
 local editor = {}
 
 editor["liubianshi/cmp-lsp-rimels"] = {
-	keys = { { "<localleader>f", mode = "i" } },
+	-- keys = { { "<localleader>f", mode = "i" } },
 	config = require("configs.editor.cmp-lsp-rimels"),
 }
+
+editor["echasnovski/mini.surround"] = {
+	version = false,
+	config = require("configs.editor.surround"),
+}
+
+editor["kealsing/im-select.nvim"] = {
+	lazy = false,
+	opts = {
+		default_im_select = "keyboard-us",
+		default_command = "fcitx5-remote",
+	},
+}
+editor["rainzm/flash-zh.nvim"] = {
+	event = "VeryLazy",
+	dependencies = "folke/flash.nvim",
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash-zh").jump({
+					chinese_only = false,
+				})
+			end,
+			desc = "Flash between Chinese",
+		},
+	},
+}
+editor["noearc/jieba.nvim"] = { dependencies = { "noearc/jieba-lua" }, opts = {} }
 
 return editor
