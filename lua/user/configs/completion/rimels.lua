d3c7eb7084dc6d3b622689e76df21674438af88a
TonyWu20 <tony.w21@gmail.com>
2025-03-03 12:06:50 +0800

    Tweaks `rime_ls`

--- lua/user/configs/completion/rimels.lua
+++ lua/user/configs/completion/rimels.lua
@@ -1,6 +1,8 @@
 return function()
 	vim.system({ "rime_ls", "--listen", "127.0.0.1:9257" }, { detach = true })
 	require("rimels").setup({
+
+		keys = { start = "<C-X>f", stop = "<C-X>;", esc = "<C-X>j", undo = "<C-X>u" },
 		cmd = vim.lsp.rpc.connect("127.0.0.1", 9257),
 		cmp_keymaps = {
 			disable = {
