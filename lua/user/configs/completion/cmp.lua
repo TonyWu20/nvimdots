d2bd61ce25229ca01f7687d4b1f2724748554724
TonyWu20 <tony.w21@gmail.com>
2025-03-03 15:43:20 +0800

    Rime `cmp` sort_order tweaks; add `nvim-focus/focus.nvim`

---
+++ lua/user/configs/completion/cmp.lua
@@ -0,0 +1,50 @@
+local compare = require("cmp.config.compare")
+compare.lsp_scores = function(entry1, entry2)
+	local diff
+	if entry1.completion_item.score and entry2.completion_item.score then
+		diff = (entry2.completion_item.score * entry2.score) - (entry1.completion_item.score * entry1.score)
+	else
+		diff = entry2.score - entry1.score
+	end
+	return (diff < 0)
+end
+local use_copilot = require("core.settings").use_copilot
+local comparators = use_copilot == true
+		and {
+			require("copilot_cmp.comparators").prioritize,
+			require("copilot_cmp.comparators").score,
+			-- require("cmp_tabnine.compare"),
+			compare.sort_text, -- 中文补全顺序显示
+			compare.offset, -- Items closer to cursor will have lower priority
+			compare.exact,
+			-- compare.scopes,
+			compare.lsp_scores,
+			compare.score,
+			compare.recently_used,
+			-- compare.locality, -- Items closer to cursor will have higher priority, conflicts with `offset`
+			require("cmp-under-comparator").under,
+			compare.kind,
+			compare.length,
+			compare.order,
+		}
+	or {
+		-- require("cmp_tabnine.compare"),
+		compare.sort_text, -- 中文补全顺序显示
+		compare.offset, -- Items closer to cursor will have lower priority
+		compare.exact,
+		-- compare.scopes,
+		compare.lsp_scores,
+		compare.score,
+		compare.recently_used,
+		-- compare.locality, -- Items closer to cursor will have higher priority, conflicts with `offset`
+		require("cmp-under-comparator").under,
+		compare.kind,
+		compare.length,
+		compare.order,
+	}
+
+return {
+	sorting = {
+		comparators = comparators,
+	},
+}
---
+++ lua/user/plugins/ui.lua
@@ -0,0 +1,7 @@
+local ui = {}
+
+ui["nvim-focus/focus.nvim"] = {
+	config = require("configs.ui.focus"),
+}
+
+return ui
