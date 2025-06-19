93ebc4fccd991ed5a0441af0443f4b4d3be6cff0
TonyWu20 <tony.w21@gmail.com>
2025-06-19 19:53:24 +0800

    Add my config for markdown-preview

---
+++ lua/user/configs/lang/markdown-preview.lua
@@ -0,0 +1,4 @@
+return function()
+	-- use a custom port to start server or empty for random
+	vim.g.mkdp_port = 3830
+end
--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -1,3 +1,9 @@
 local lang = {}
 lang["tommason14/lammps.vim"] = {}
+lang["iamcco/markdown-preview.nvim"] = {
+	lazy = true,
+	ft = "markdown",
+	build = ":call mkdp#util#install()",
+	config = require("configs.lang.markdown-preview"),
+}
 return lang
