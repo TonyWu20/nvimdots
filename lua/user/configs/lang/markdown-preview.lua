b499ca3c265ab4e6a886e8f4eb7112bb1eac797a
TonyWu20 <tony.w21@gmail.com>
2025-06-23 10:18:51 +0800

    Try markdown-preview remote view

--- lua/user/configs/lang/markdown-preview.lua
+++ lua/user/configs/lang/markdown-preview.lua
@@ -1,5 +1,8 @@
 return function()
 	-- use a custom port to start server or empty for random
-	vim.g.mkdp_port = 3000
+	vim.g.mkdp_open_ip = "127.0.0.1"
 	vim.g.mkdp_open_to_the_world = 1
+	vim.g.mkdp_port = 8080
+	vim.g.mkdp_browser = "none"
+	vim.g.mkdp_echo_preview_url = 1
 end
