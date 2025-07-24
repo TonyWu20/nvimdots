f8e25e9b44bac9d7721ff50214b2eac2e90ad856
TonyWu20 <tony.w21@gmail.com>
2025-07-25 03:23:11 +0800

    ignore `.direnv` for `rust-analyzer`

--- lua/user/configs/lang/rust.lua
+++ lua/user/configs/lang/rust.lua
@@ -1,31 +1,33 @@
-return function()
-	vim.g.rustaceanvim = {
-		-- Disable automatic DAP configuration to avoid conflicts with previous user configs
-		dap = {
-			adapter = false,
-			configuration = false,
-			autoload_configurations = false,
-		},
-		server = {
-			default_settings = {
-				["rust-analyzer"] = {
-					checkOnSave = {
-						allFeatures = true,
-						command = "cargo check",
-					},
-					procMacro = {
-						enable = true,
-					},
-					completion = {
-						autoimport = true,
-					},
-					files = {
-						excludeDirs = { ".direnv" },
+return {
+	defaults = function()
+		vim.g.rustaceanvim = {
+			-- Disable automatic DAP configuration to avoid conflicts with previous user configs
+			dap = {
+				adapter = false,
+				configuration = false,
+				autoload_configurations = false,
+			},
+			server = {
+				default_settings = {
+					["rust-analyzer"] = {
+						checkOnSave = {
+							allFeatures = true,
+							command = "cargo check",
+						},
+						procMacro = {
+							enable = true,
+						},
+						completion = {
+							autoimport = true,
+						},
+						files = {
+							excludeDirs = { ".direnv" },
+						},
 					},
 				},
 			},
-		},
-	}
+		}
 
-	require("modules.utils").load_plugin("rustaceanvim", nil, true)
-end
+		require("modules.utils").load_plugin("rustaceanvim", nil, true)
+	end,
+}
