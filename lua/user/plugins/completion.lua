a0d3a6d8cefcb48d2f3349f89f575ea12f4d7c1f
TonyWu20 <tony.w21@gmail.com>
2026-03-02 19:21:09 +0800

    Enable `rime-ls`

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -1,11 +1,11 @@
 local completion = {}
--- completion["hrsh7th/nvim-cmp"] = {
--- 	dependencies = {
--- 		{
--- 			"liubianshi/cmp-lsp-rimels",
--- 			config = require("configs.completion.rimels"),
--- 		},
--- 	},
--- }
+completion["hrsh7th/nvim-cmp"] = {
+	dependencies = {
+		{
+			"liubianshi/cmp-lsp-rimels",
+			config = require("configs.completion.rimels"),
+		},
+	},
+}
 
 return completion
