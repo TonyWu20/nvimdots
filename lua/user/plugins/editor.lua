23d69aa206d97d2f0511476efce930b6683bab58
TonyWu20 <tony.w21@gmail.com>
2026-04-23 18:08:51 +0800

    Fix incorrect repo name for `im-select`

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -5,7 +5,7 @@ editor["echasnovski/mini.surround"] = {
 	config = require("configs.editor.surround"),
 }
 
-editor["kealsing/im-select.nvim"] = {
+editor["keaising/im-select.nvim"] = {
 	lazy = false,
 	opts = {
 		default_im_select = "keyboard-us",
