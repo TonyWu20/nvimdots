8a0e89184834f4717c7023665c87307fbd7f74cb
TonyWu20 <tony.w21@gmail.com>
2026-05-04 22:21:23 +0800

    Migration to 0.12 with treesitter changes

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -39,4 +39,69 @@ editor["kkew3/jieba.vim"] = {
 	end,
 }
 
+editor["nvim-treesitter/nvim-treesitter"] = {
+	lazy = false,
+	branch = "main",
+	build = function()
+		if #vim.api.nvim_list_uis() > 0 then
+			vim.api.nvim_command([[TSUpdate]])
+		end
+	end,
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				-- Enable treesitter highlighting and disable regex syntax
+				pcall(vim.treesitter.start)
+				-- Enable treesitter-based indentation
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+		local ensureInstalled = {
+			"lua",
+			"python",
+			"rust",
+			"latex",
+			"markdown",
+			"fortran",
+			-- ... your parsers
+		}
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+		require("nvim-treesitter").install(parsersToInstall)
+		-- ...
+	end,
+	dependencies = {
+		{ "mfussenegger/nvim-treehopper" },
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			branch = "main",
+			config = require("editor.ts-textobjects"),
+		},
+		{
+			"andymass/vim-matchup",
+			init = require("editor.matchup"),
+		},
+		{
+			"windwp/nvim-ts-autotag",
+			config = require("editor.autotag"),
+		},
+		{
+			"hiphish/rainbow-delimiters.nvim",
+			submodules = false,
+			config = require("editor.rainbow_delims"),
+		},
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+			config = require("editor.ts-context"),
+		},
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			config = require("editor.ts-context-commentstring"),
+		},
+	},
+}
 return editor
